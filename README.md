7e10429fc4385bb89fe958d5a71b2b3cd48ddeaa
Aaron Platt <aaron.k.platt@gmail.com>
2020-11-19 18:36:03 -0800

    I got the routes to start working

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Unit 20 React Homework: React Portfolio
+# https://aaronkplatt.github.io/aaronkplatt-portfolio.github.io/
+
+# COLORS 
+tea green: #D1F0B1
+Laurel Green: #B6CB9E
+Cambridge Blue: #92B4A7
+Taupe Gray: #8C8A93
+Old Lavender: #81667A
 
 Now that you've worked with React and have multiple projects to share, you'll be updating your portfolio and other materials to build toward being employer competitive. Creating a portfolio using React will help set you apart from other developers whose portfolios do not use some of the latest technologies.
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Router, Route } from 'react-router-dom';
+//PAGES
+import Home from './pages/home';
+import Portfolio from './pages/portfolio';
+import Contact from './pages/contact';
+
+//COMPONENTS
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/about" component={Portfolio}></Route>
+          <Route exact path="/about" component={Contact}></Route>
+        </Switch>
+        <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
